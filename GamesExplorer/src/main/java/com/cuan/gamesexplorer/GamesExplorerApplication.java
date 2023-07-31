8bfb2339b676603cee840296d503aa6291a18abb
Cuan Otto <cuan.otto@iotnxt.com>
2023-07-31 10:22:01 +0200

    Spring boot initialized projecty

---
+++ GamesExplorer/src/main/java/com/cuan/gamesexplorer/GamesExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.cuan.gamesexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesExplorerApplication.class, args);
+	}
+
+}
---
+++ GamesExplorer/src/test/java/com/cuan/gamesexplorer/GamesExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuan.gamesexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
