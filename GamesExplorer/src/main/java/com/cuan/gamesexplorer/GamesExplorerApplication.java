62d179c6803704be5d34f4b6b0078d58904e4a06
Cuan Otto <cuan.otto@iotnxt.com>
2023-07-31 15:59:12 +0200

    Add Caching Add Scheduled Cache Eviction

--- GamesExplorer/src/main/java/com/cuan/gamesexplorer/GamesExplorerApplication.java
+++ GamesExplorer/src/main/java/com/cuan/gamesexplorer/GamesExplorerApplication.java
@@ -2,8 +2,10 @@ package com.cuan.gamesexplorer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GamesExplorerApplication {
 
 	public static void main(String[] args) {
